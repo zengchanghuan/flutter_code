0fdc5b822f841934c824af47b37bd76fb8e249a3
zengchanghuan <zengchanghuan@gmail.com>
2022-04-24 10:11:48 +0800

    add columns

--- flutter_demo/lib/main.dart
+++ flutter_demo/lib/main.dart
@@ -1,26 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-import './pages/index_page.dart';
+void main() => runApp(MaterialApp(
+  home:Home()
 
+));
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: MaterialApp(
-        title: 'tristan',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryColor: Colors.pink
-        ),
-        home: IndexPage(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('tristan'),
+        centerTitle: true,
+        backgroundColor: Colors.red[10],
+      ),
+      body:Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Row(
+            children: [
+              Text('hello'),
+              Text('world'),
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            color: Colors.cyan,
+            child: Text('one'),
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            color: Colors.cyan,
+            child: Text('two'),
+          ),
+          Container(
+            padding: EdgeInsets.all(30.0),
+            color: Colors.cyan,
+            child: Text('three'),
+          ),
+          Container(
+            padding: EdgeInsets.all(40.0),
+            color: Colors.cyan,
+            child: Text('four'),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Text('click'),
+        backgroundColor: Colors.red[600],
       ),
     );
   }
 }
+
--- flutter_demo/lib/pages/home_page.dart
+++ flutter_demo/lib/pages/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:dio/dio.dart';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
